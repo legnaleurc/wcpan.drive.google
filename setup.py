75f168f6078a1b8a6e9ba5f0cd101d0d9f703646
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-06-10 01:28:58 +0900

    add setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+        name='wcpan.drive.google',
+        version='1.0.0.dev1',
+        author='Wei-Cheng Pan',
+        author_email='legnaleurc@gmail.com',
+        description='Google Drive API with Tornado.',
+        url='https://github.com/legnaleurc/wcpan.drive.google',
+        packages=[
+            'wcpan.drive.google',
+        ],
+        install_requires=[
+            'PyDrive',
+            'tornado',
+            'wcpan.logger',
+        ],
+        classifiers=[
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3.5',
+        ])
