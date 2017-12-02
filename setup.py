0470e8df45d29bf2e85f1553e546b3f6822f015f
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-12-02 16:03:20 +0900

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
         name='wcpan.drive.google',
-        version='1.0.0.dev2',
+        version='1.0.0.dev3',
         author='Wei-Cheng Pan',
         author_email='legnaleurc@gmail.com',
         description='Google Drive API with Tornado.',
