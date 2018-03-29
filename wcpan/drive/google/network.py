a402cb944a7b42a2cb540fe3f38f24f228115484
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-03-29 21:59:38 +0800

    rewrite network module

--- wcpan/drive/google/network.py
+++ wcpan/drive/google/network.py
@@ -1,9 +1,10 @@
+import asyncio
 import json
 import math
 import random
 import urllib.parse as up
 
-from tornado import httpclient as thc, httputil as thu, gen as tg
+import aiohttp
 from wcpan.logger import DEBUG, EXCEPTION, INFO, WARNING
 
 from .util import GoogleDriveError
@@ -17,19 +18,19 @@ class Network(object):
 
     def __init__(self):
         self._access_token = None
-        self._http = thc.AsyncHTTPClient()
         self._backoff_level = 0
+        self._session = aiohttp.ClientSession()
 
     def set_access_token(self, token):
         self._access_token = token
 
     async def fetch(self, method, path, args=None, headers=None, body=None,
-                    consumer=None, raise_internal_error=False):
+                    raise_internal_error=False):
         while True:
             await self._maybe_backoff()
             try:
                 rv = await self._do_request(method, path, args, headers, body,
-                                            consumer, raise_internal_error)
+                                            raise_internal_error)
                 return rv
             except NetworkError as e:
                 if e.status != '599':
@@ -38,35 +39,27 @@ class Network(object):
                     raise
                 WARNING('wcpan.drive.google') << str(e)
 
-    async def _do_request(self, method, path, args, headers, body, consumer,
+    async def _do_request(self, method, path, args, headers, body,
                           raise_internal_error):
         headers = self._prepare_headers(headers)
-        if args is not None:
-            path = thu.url_concat(path, args)
 
-        args = {
-            'url': path,
+        kwargs = {
             'method': method,
+            'url': url,
             'headers': headers,
         }
+        if args is not None:
+            kwargs['params'] = list(normalize_query_string(args))
         if body is not None:
-            if callable(body):
-                args['body_producer'] = body
-            else:
-                args['body'] = body
-        elif method == 'PUT':
-            # for resume uploads
-            args['allow_nonstandard_methods'] = True
-        if consumer is not None:
-            args['streaming_callback'] = consumer
+            kwargs['data'] = body if not callable(body) else body()
         if raise_internal_error:
             # do not raise timeout from client
-            args['request_timeout'] = 0.0
+            kwargs['timeout'] = 0
 
-        request = thc.HTTPRequest(**args)
-        rv = await self._http.fetch(request, raise_error=False)
-        rv = Response(rv, raise_internal_error)
-        rv = self._handle_status(rv)
+        response = await self._session.request(**kwargs)
+
+        rv = Response(response, raise_internal_error)
+        rv = await self._handle_status(rv)
         return rv
 
     def _prepare_headers(self, headers):
@@ -79,8 +72,9 @@ class Network(object):
              for k, v in h.items()}
         return h
 
-    def _handle_status(self, response):
-        if backoff_needed(response):
+    async def _handle_status(self, response):
+        backoff = await backoff_needed(response)
+        if backoff:
             self._increase_backoff_level()
         else:
             self._decrease_backoff_level()
@@ -90,7 +84,8 @@ class Network(object):
             return response
 
         # otherwise it is an error
-        raise NetworkError(response)
+        json_ = await response.json()
+        raise NetworkError(response, json_, not backoff)
 
     def _increase_backoff_level(self):
         self._backoff_level = min(self._backoff_level + 2, 10)
@@ -106,7 +101,7 @@ class Network(object):
         s_delay = math.floor(seed * power * BACKOFF_FACTOR)
         s_delay = min(100, s_delay)
         DEBUG('wcpan.drive.google') << 'backoff for' << s_delay
-        await tg.sleep(s_delay)
+        await asyncio.sleep(s_delay)
 
 
 class Request(object):
@@ -132,8 +127,8 @@ class Response(object):
     def __init__(self, response, raise_internal_error):
         self._response = response
         self._raise_internal_error = raise_internal_error
-        self._request = Request(response.request)
-        self._status = str(self._response.code)
+        self._request = Request(response.request_info)
+        self._status = str(response.status)
         self._parsed_json = False
         self._json = None
 
@@ -145,47 +140,48 @@ class Response(object):
     def reason(self):
         return self._response.reason
 
-    @property
-    def json_(self):
-        if not self._parsed_json:
-            rv = self._response.body
-            if not rv:
-                rv = None
-            else:
-                rv = rv.decode('utf-8')
-                try:
-                    rv = json.loads(rv)
-                except ValueError as e:
-                    EXCEPTION('wcpan.drive.google') << rv
-                    rv = None
-            self._json = rv
-            self._parsed_json = True
+    async def json(self):
+        if self._parsed_json:
+            return self._json
+
+        try:
+            rv = await self._response.json()
+        except aiohttp.ContentTypeError as e:
+            EXCEPTION('wcpan.drive.google') << rv
+            rv = None
+        except ValueError as e:
+            EXCEPTION('wcpan.drive.google') << rv
+            rv = None
+
+        self._json = rv
+        self._parsed_json = True
+
         return self._json
+
+    def chunks(self):
+        return self._response.content.iter_any()
 
     @property
     def request(self):
         return self._request
 
     @property
-    def error(self):
-        return self._response.error
-
-    @property
     def raise_internal_error(self):
         return self._raise_internal_error
 
     def get_header(self, key):
-        h = self._response.headers.get_list(key)
+        h = self._response.headers.getall(key)
         return None if not h else h[0]
 
 
 class NetworkError(GoogleDriveError):
 
-    def __init__(self, response):
+    def __init__(self, response, json_, fatal):
         self._response = response
         self._message = '{0} {1} - {2}'.format(self.status,
                                                self._response.reason,
-                                               self.json_)
+                                               json_)
+        self._fatal = fatal
 
     def __str__(self):
         return self._message
@@ -196,31 +192,20 @@ class NetworkError(GoogleDriveError):
 
     @property
     def fatal(self):
-        return not backoff_needed(self._response)
+        return self._fatal
 
     @property
-    def json_(self):
-        return self._response.json_
-
-    @property
-    def error(self):
-        return self._response.error
+    def json(self):
+        return self._json
 
 
-def initialize():
-    args = {
-        'max_body_size': 10 * (1024 ** 3),
-    }
-    thc.AsyncHTTPClient.configure(None, **args)
-
-
-def backoff_needed(response):
+async def backoff_needed(response):
     if response.status not in BACKOFF_STATUSES:
         return False
 
     # if it is not a rate limit error, it could be handled immediately
     if response.status == '403':
-        msg = response.json_
+        msg = await response.json()
         if not msg:
             WARNING('wcpan.drive.google') << '403 with empty error message'
             # probably server problem, backoff for safety
@@ -233,4 +218,12 @@ def backoff_needed(response):
     return True
 
 
-initialize()
+def normalize_query_string(qs):
+    for key, value in qs.items():
+        if isinstance(value, bool):
+            value = 'true' if value else 'false'
+        elif isinstance(value, (int, float)):
+            value = str(value)
+        elif not isinstance(value, str):
+            raise ValueError('unknown type in query string')
+        yield key, value
