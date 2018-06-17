a5d82772c23a724f96e55da06cfe99a16d54c477
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-06-17 10:35:25 +0900

    refactor classes

--- wcpan/drive/google/__main__.py
+++ wcpan/drive/google/__main__.py
@@ -67,7 +67,7 @@ async def verify_upload_file(drive, local_path, remote_node):
     wl.INFO('wcpan.drive.google') << 'ok : {0}'.format(local_path)
 
 
-class UploadQueue(object):
+class AbstractQueue(object):
 
     def __init__(self, drive):
         self._drive = drive
@@ -77,218 +77,196 @@ class UploadQueue(object):
         self._total = 0
         self._failed = []
 
-    async def upload(self, local_path_list, parent_node):
-        self._counter = 0
-        self._total = sum(self._count_tasks(_) for _ in local_path_list)
-        for local_path in local_path_list:
-            fn = ft.partial(self._internal_upload, local_path, parent_node)
-            self._push(fn)
-        await self._wait_for_complete()
+    @property
+    def drive(self):
+        return self._drive
 
     @property
     def failed(self):
         return self._failed
 
-    def _count_tasks(self, local_path):
+    async def run(self, src_list, dst):
+        self._counter = 0
+        total = (self.count_tasks(_) for _ in src_list)
+        total = await asyncio.gather(*total)
+        self._total = sum(total)
+        for src in src_list:
+            self._push_task(self._run_one_task(src, dst))
+        await self._wait_for_complete()
+
+    async def count_tasks(self, src):
+        raise NotImplementedError()
+
+    def source_is_folder(self, src):
+        raise NotImplementedError()
+
+    async def do_folder(self, src, dst):
+        raise NotImplementedError()
+
+    async def get_children(self, src):
+        raise NotImplementedError()
+
+    async def do_file(self, src, dst):
+        raise NotImplementedError()
+
+    async def _run_one_task(self, src, dst):
+        if self.source_is_folder(src):
+            rv = await self._run_for_folder(src, dst)
+        else:
+            rv = await self._run_for_file(src, dst)
+        return rv
+
+    async def _run_for_folder(self, src, dst):
+        await self._log_begin(src)
+        try:
+            rv = await self.do_folder(src, dst)
+        except Exception as e:
+            self._add_failed(src)
+            rv = None
+        await self._log_end(rv)
+
+        if not rv:
+            return None
+
+        children = await self.get_children(src)
+        for child in children:
+            self._push_task(self._run_one_task(child, rv))
+
+        return rv
+
+    async def _run_for_file(self, src, dst):
+        await self._log_begin(src)
+        try:
+            rv = await self.do_file(src, dst)
+        except Exception as e:
+            self._add_failed(src)
+            rv = None
+        await self._log_end(rv)
+        return rv
+
+    async def _wait_for_complete(self):
+        async with self._final:
+            await self._final.wait()
+
+    def _push_task(self, future):
+        future = self._task_runner(future)
+        loop = asyncio.get_event_loop()
+        loop.create_task(future)
+
+    async def _task_runner(self, future):
+        async with self._lock:
+            await future
+            self._counter += 1
+            if self._counter == self._total:
+                async with self._final:
+                    self._final.notify()
+
+    def _add_failed(self, src):
+        self._failed.append(src)
+
+    async def _log_begin(self, node_or_path):
+        await self._log('begin', node_or_path)
+
+    async def _log_end(self, node_or_path):
+        await self._log('end', node_or_path)
+
+    async def _log(self, begin_or_end, node_or_path):
+        progress = self._get_progress()
+        if node_or_path is None:
+            node_or_path = '__FATAL_ERROR__'
+        elif not isinstance(node_or_path, str):
+            node_or_path = await self._drive.get_path(node_or_path)
+        wl.INFO('wcpan.drive.google') << '{0} {1} {2}'.format(progress, begin_or_end, node_or_path)
+
+    def _get_progress(self):
+        return '[{0}/{1}]'.format(self._counter, self._total)
+
+
+class UploadQueue(AbstractQueue):
+
+    def __init__(self, drive):
+        super(UploadQueue, self).__init__(drive)
+
+    async def count_tasks(self, local_path):
         total = 1
         for root, folders, files in os.walk(local_path):
             total = total + len(folders) + len(files)
         return total
 
-    async def _wait_for_complete(self):
-        async with self._final:
-            await self._final.wait()
+    def source_is_folder(self, local_path):
+        return op.isdir(local_path)
 
-    async def _internal_upload(self, local_path, parent_node):
-        if op.isdir(local_path):
-            rv = await self._retry_create_folder(local_path, parent_node)
-        else:
-            rv = await self._retry_upload_file(local_path, parent_node)
-        return rv
-
-    async def _retry_create_folder(self, local_path, parent_node):
-        await self._log_begin(local_path)
-
+    async def do_folder(self, local_path, parent_node):
         folder_name = op.basename(local_path)
         while True:
             try:
-                node = await self._drive.create_folder(parent_node, folder_name,
-                                                       exist_ok=True)
+                node = await self.drive.create_folder(parent_node, folder_name,
+                                                      exist_ok=True)
                 break
             except NetworkError as e:
                 wl.EXCEPTION('wcpan.drive.google', e)
                 if e.status not in ('599',) and e.fatal:
-                    self._add_failed(local_path)
                     raise
-
-        await self._log_end(node)
-
-        for child_path in os.listdir(local_path):
-            child_path = op.join(local_path, child_path)
-            fn = ft.partial(self._internal_upload, child_path, node)
-            self._push(fn)
-
         return node
 
-    async def _retry_upload_file(self, local_path, parent_node):
-        await self._log_begin(local_path)
+    async def get_children(self, local_path):
+        rv = os.listdir(local_path)
+        rv = (op.join(local_path, _) for _ in rv)
+        return rv
 
+    async def do_file(self, local_path, parent_node):
         while True:
             try:
-                node = await self._drive.upload_file(local_path, parent_node,
-                                                     exist_ok=True)
+                node = await self.drive.upload_file(local_path, parent_node,
+                                                    exist_ok=True)
                 break
             except NetworkError as e:
                 wl.EXCEPTION('wcpan.drive.google', e)
                 if e.status not in ('599',) and e.fatal:
-                    self._add_failed(local_path)
                     raise
-
-        await self._log_end(node)
-
         return node
 
-    def _push(self, runnable):
-        loop = asyncio.get_event_loop()
-        t = self._do_upload_task(runnable)
-        loop.create_task(t)
 
-    async def _do_upload_task(self, runnable):
-        async with self._lock:
-            await runnable()
-            self._counter = self._counter + 1
-            if self._counter == self._total:
-                async with self._final:
-                    self._final.notify()
-
-    def _add_failed(self, local_path):
-        self._failed.append(local_path)
-
-    async def _log_begin(self, local_path):
-        progress = self._get_progress()
-        wl.INFO('wcpan.drive.google') << '{0} begin {1}'.format(progress, local_path)
-
-    async def _log_end(self, remote_node):
-        progress = self._get_progress()
-        remote_path = await self._drive.get_path(remote_node)
-        wl.INFO('wcpan.drive.google') << '{0} end {1}'.format(progress, remote_path)
-
-    def _get_progress(self):
-        return '[{0}/{1}]'.format(self._counter, self._total)
-
-
-class DownloadQueue(object):
+class DownloadQueue(AbstractQueue):
 
     def __init__(self, drive):
-        self._drive = drive
-        self._lock = asyncio.Semaphore(value=8)
-        self._final = asyncio.Condition()
-        self._counter = 0
-        self._total = 0
-        self._failed = []
+        super(DownloadQueue, self).__init__(drive)
 
-    async def download(self, node_list, local_path):
-        self._counter = 0
-        total = (self._count_tasks(_) for _ in node_list)
-        total = await asyncio.gather(*total)
-        self._total = sum(total)
-        for node in node_list:
-            if node.is_trashed:
-                continue
-            fn = ft.partial(self._internal_download, node, local_path)
-            self._push(fn)
-        await self._wait_for_complete()
-
-    @property
-    def failed(self):
-        return self._failed
-
-    async def _count_tasks(self, node):
+    async def count_tasks(self, node):
         total = 1
-        children = await self._drive.get_children(node)
-        count = (self._count_tasks(_) for _ in children)
+        children = await self.drive.get_children(node)
+        count = (self.count_tasks(_) for _ in children)
         count = await asyncio.gather(*count)
         return total + sum(count)
 
-    async def _wait_for_complete(self):
-        async with self._final:
-            await self._final.wait()
+    def source_is_folder(self, node):
+        return node.is_folder
 
-    async def _internal_download(self, node, local_path):
-        if node.is_folder:
-            rv = await self._create_folder(node, local_path)
-        else:
-            rv = await self._retry_download_file(node, local_path)
-        return rv
-
-    async def _create_folder(self, node, local_path):
-        await self._log_begin(node)
-
+    async def do_folder(self, node, local_path):
         full_path = op.join(local_path, node.name)
         try:
             os.makedirs(full_path, exist_ok=True)
         except Exception as e:
             wl.EXCEPTION('wcpan.drive.google', e)
-            self._add_failed(node)
-
-        await self._log_end(full_path)
-
-        children = await self._drive.get_children(node)
-        for child in children:
-            fn = ft.partial(self._internal_download, child, full_path)
-            self._push(fn)
-
+            raise
         return full_path
 
-    async def _retry_download_file(self, node, local_path):
-        await self._log_begin(node)
+    async def get_children(self, node):
+        return await self.drive.get_children(node)
 
+    async def do_file(self, node, local_path):
         while True:
             try:
-                rv = await self._drive.download_file(node, local_path)
+                rv = await self.drive.download_file(node, local_path)
                 break
             except DownloadError as e:
                 wl.EXCEPTION('wcpan.drive.google', e)
-                self._add_failed(node)
                 raise
             except NetworkError as e:
                 wl.EXCEPTION('wcpan.drive.google', e)
                 if e.status not in ('599',) and e.fatal:
-                    self._add_failed(node)
                     raise
-
-        full_path = op.join(local_path, node.name)
-        await self._log_end(full_path)
-
         return rv
-
-    def _push(self, runnable):
-        loop = asyncio.get_event_loop()
-        t = self._do_download_task(runnable)
-        loop.create_task(t)
-
-    async def _do_download_task(self, runnable):
-        async with self._lock:
-            await runnable()
-            self._counter = self._counter + 1
-            if self._counter == self._total:
-                async with self._final:
-                    self._final.notify()
-
-    def _add_failed(self, local_path):
-        self._failed.append(local_path)
-
-    async def _log_begin(self, remote_node):
-        progress = self._get_progress()
-        remote_path = await self._drive.get_path(remote_node)
-        wl.INFO('wcpan.drive.google') << '{0} begin {1}'.format(progress, remote_path)
-
-    async def _log_end(self, local_path):
-        progress = self._get_progress()
-        wl.INFO('wcpan.drive.google') << '{0} end {1}'.format(progress, local_path)
-
-    def _get_progress(self):
-        return '[{0}/{1}]'.format(self._counter, self._total)
 
 
 async def main(args=None):
