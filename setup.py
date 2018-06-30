25230b04b14cebe0ad63520ac9bb034bd5eb0230
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-06-30 21:34:29 +0900

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
         name='wcpan.drive.google',
-        version='2.1.1',
+        version='2.1.2',
         author='Wei-Cheng Pan',
         author_email='legnaleurc@gmail.com',
         description='Asynchronous Google Drive API.',
