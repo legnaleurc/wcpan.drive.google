3bc95e596541c6e364e3130f25b5a7a252c9c330
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-01-18 19:45:04 +0900

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(op.join(op.dirname(__file__), './README.md')) as fin:
 
 setup(
         name='wcpan.drive.google',
-        version='4.0.2',
+        version='4.0.3',
         author='Wei-Cheng Pan',
         author_email='legnaleurc@gmail.com',
         description='Asynchronous Google Drive API.',
