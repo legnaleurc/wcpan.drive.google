8575f8f7922f35c3f63ebafcc1e75a5794be5104
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-12-13 01:57:03 +0900

    update setup script

--- setup.py
+++ setup.py
@@ -1,33 +1,4 @@
-import os.path as op
-
 from setuptools import setup
 
 
-with open(op.join(op.dirname(__file__), './README.md')) as fin:
-    long_description = fin.read()
-
-
-setup(
-        name='wcpan.drive.google',
-        version='5.0.0',
-        author='Wei-Cheng Pan',
-        author_email='legnaleurc@gmail.com',
-        description='Asynchronous Google Drive API.',
-        long_description=long_description,
-        long_description_content_type='text/markdown',
-        url='https://github.com/legnaleurc/wcpan.drive.google',
-        packages=[
-            'wcpan.drive.google',
-        ],
-        python_requires='>= 3.8',
-        install_requires=[
-            'PyYAML >= 3.12',
-            'aiohttp >= 3.4.4',
-            'arrow >= 0.12.1',
-            'wcpan.logger >= 1.2.3',
-            'wcpan.worker >= 4.1.2',
-        ],
-        classifiers=[
-            'Programming Language :: Python :: 3 :: Only',
-            'Programming Language :: Python :: 3.8',
-        ])
+setup()
--- wcpan/drive/google/__init__.py
+++ wcpan/drive/google/__init__.py
@@ -1,1 +1,3 @@
+__version__ = '6.0.0'
+
 from .drive import GoogleDriver as Driver
